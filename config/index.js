6ad1822a84ff2ed8d6dba7c0a21507af6eecb9d1
Quin1ann <112182956+Quin1ann@users.noreply.github.com>
2022-08-26 22:14:34 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ export const config = {
     // 模板消息id
     templateId: "c9zHKtJ3BrNmBINWdBWiVg8BUAJQ4MW2y1UCj7A9CJk",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: ["od_A16tmSxdpeJlfTD5RHI64A8dQ"],
+    user: ["od_A16nice08Alc0dhJrsnDwsWww"],
      
     // 信息配置
     // 所在省份
@@ -30,7 +30,7 @@ export const config = {
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
 // 今天是我们恋爱的第{{love_day.DATA}}天
-// 今天是你正式工作的的第{{marry_day.DATA}}天
+// 今天是你正式工作的的第{{marry_day.DATA}}天,要记得好好吃饭哦~
 // {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
